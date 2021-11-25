b3a3cb16dd159973c27ed268d473db6097b764a9
basilnarakodan <66983063+basilnarakodan@users.noreply.github.com>
2021-11-25 19:48:24 +0530

    Update 22_fibonacci.py

--- 22_fibonacci.py
+++ 22_fibonacci.py
@@ -1,14 +1,12 @@
 def fib(n):
     a=0
     b=1
-    print(a,b)
-    for i in range(n):
+    print(a,b,end=" ")
+    for i in range(n-2):  #n-2 prints n elements.give while(c<n) to print upto n elements
         c=a+b
-        print(c)
+        print(c,end=" ")
         a=b
         b=c
 
-
-a=int(input("enter limit: "))
-fib(a)
+fib(int(input("enter limit: ")))
         
