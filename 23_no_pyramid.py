2387345aff5f304838aab06fe2361fd7516432d3
basilnarakodan <66983063+basilnarakodan@users.noreply.github.com>
2021-11-25 19:32:07 +0530

    Update 23_no_pyramid.py

--- 23_no_pyramid.py
+++ 23_no_pyramid.py
@@ -1,5 +1,5 @@
-a=int(input("enter limit: "))
+a=int(input("enter number of levels: "))
 for i in range(1,a+1):
     for j in range(1,i+1):
-        print(i*j)
+        print(i*j,end="\t")
     print("\n")
