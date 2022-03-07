62e5a659425fd18bd8507e859d1582c99f325735
basilnarakodan <66983063+basilnarakodan@users.noreply.github.com>
2022-03-07 15:03:38 +0530

    Update 18_no_reverse.py

--- 18_no_reverse.py
+++ 18_no_reverse.py
@@ -1,7 +1,10 @@
-a=int(input("enter a number"))
-rev=0
-while(a>0):
-    digit=a%10
-    rev=digit+rev*10
-    a=a//10
-print(rev)
+a=int(input("enter no: "))
+f=0
+for i in range(1,a):
+    if(a%i==0):
+        f=f+1
+if f>1:
+    print("not prime")
+else:
+    print("prime")
+    
